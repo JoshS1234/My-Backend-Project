179bd6a47d5b8db4c0dc0dc6937dfa82f9a7ff10
Joshua Spence <joshua.spence@hotmail.co.uk>
2022-09-05 14:52:48 +0100

    removed catch blocks

--- controllers/gamesC.js
+++ controllers/gamesC.js
@@ -1,11 +1,7 @@
 const { getCategoryList } = require("../models/gamesM.js");
 
 exports.getCategories = (req, res) => {
-  return getCategoryList()
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch((err) => {
-      next(err);
-    });
+  return getCategoryList().then((data) => {
+    res.status(200).send(data);
+  });
 };
--- models/gamesM.js
+++ models/gamesM.js
@@ -1,12 +1,7 @@
 const db = require("../db/connection");
 
 exports.getCategoryList = () => {
-  return db
-    .query("SELECT * FROM categories")
-    .then((data) => {
-      return data.rows;
-    })
-    .catch((err) => {
-      return Promise.reject(err);
-    });
+  return db.query("SELECT * FROM categories").then((data) => {
+    return data.rows;
+  });
 };
