db242d8ec08e0f479a13b6941306aa2a41bc89a1
Joshua Spence <joshua.spence@hotmail.co.uk>
2022-09-05 16:00:40 +0100

    worked through the review_id task (task 4), including TDD and error handling

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
-const { getCategories } = require(`${__dirname}/controllers/gamesC`);
+const {
+  getCategories,
+  getReviewByID,
+} = require(`${__dirname}/controllers/gamesC`);
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewByID);
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send(err.msg);
+  }
+
+  let errorPSQLCodes = ["22P02"];
+  if (errorPSQLCodes.includes(err.code)) {
+    res.status(400).send({ msg: "bad request" });
+  }
+  next(err);
+});
 
 //error handling
 app.use((err, req, res, next) => {
--- controllers/gamesC.js
+++ controllers/gamesC.js
@@ -1,7 +1,18 @@
-const { getCategoryList } = require("../models/gamesM.js");
+const { getCategoryList, getSingleReviewByID } = require("../models/gamesM.js");
 
 exports.getCategories = (req, res) => {
   return getCategoryList().then((data) => {
     res.status(200).send(data);
   });
+};
+
+exports.getReviewByID = (req, res, next) => {
+  let reviewID = req.params.review_id;
+  return getSingleReviewByID(reviewID)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
