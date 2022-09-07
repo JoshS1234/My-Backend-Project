2ff2d148605fcb743c19d16f18a309a50e3cccc5
Joshua Spence <joshua.spence@hotmail.co.uk>
2022-09-07 10:47:23 +0100

    changed blah to <categoryName>

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -233,7 +233,7 @@ describe("Patch /api/reviews/:review_id", () => {
   });
 });
 
-describe("get /api/reviews?category=blah", () => {
+describe("get /api/reviews?category=<categoryName>", () => {
   test("returns an array of objects", () => {
     return request(app)
       .get("/api/reviews")
