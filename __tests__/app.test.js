ff057c134d2c3b1075f474c76ee1043870305f7f
JoshS1234 <joshua.spence@hotmail.co.uk>
2022-09-28 22:29:11 +0100

    cleaned up a bit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -244,41 +244,41 @@ describe("GET /api/reviews/:review_id/comments", () => {
       });
   });
 
-  // test("Returned objects are fully correct", () => {
-  //   let correct = [
-  //     {
-  //       comment_id: 5,
-  //       body: "Now this is a story all about how, board games turned my life upside down",
-  //       review_id: 2,
-  //       author: "mallionaire",
-  //       votes: 13,
-  //       created_at: "2021-01-18T10:24:05.410Z",
-  //     },
+  test("Returned objects are fully correct", () => {
+    let correct = [
+      {
+        comment_id: 5,
+        body: "Now this is a story all about how, board games turned my life upside down",
+        review_id: 2,
+        author: "mallionaire",
+        votes: 13,
+        created_at: "2021-01-18T10:24:05.410Z",
+      },
 
-  //     {
-  //       comment_id: 1,
-  //       body: "I loved this game too!",
-  //       review_id: 2,
-  //       author: "bainesface",
-  //       votes: 16,
-  //       created_at: "2017-11-22T12:43:33.389Z",
-  //     },
-  //     {
-  //       comment_id: 4,
-  //       body: "EPIC board game!",
-  //       review_id: 2,
-  //       author: "bainesface",
-  //       votes: 16,
-  //       created_at: "2017-11-22T12:36:03.389Z",
-  //     },
-  //   ];
-  //   return request(app)
-  //     .get("/api/reviews/2/comments")
-  //     .expect(200)
-  //     .then((res) => {
-  //       expect(res.body.comments).toEqual(correct);
-  //     });
-  // });
+      {
+        comment_id: 1,
+        body: "I loved this game too!",
+        review_id: 2,
+        author: "bainesface",
+        votes: 16,
+        created_at: "2017-11-22T12:43:33.389Z",
+      },
+      {
+        comment_id: 4,
+        body: "EPIC board game!",
+        review_id: 2,
+        author: "bainesface",
+        votes: 16,
+        created_at: "2017-11-22T12:36:03.389Z",
+      },
+    ];
+    return request(app)
+      .get("/api/reviews/2/comments")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.comments).toEqual(correct);
+      });
+  });
 });
 
 describe("get /api/reviews?category=<categoryName>&sort_by=<sort_category>&order=<ASC/DESC>", () => {
